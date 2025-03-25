7aefbf5878e134c410154873395ed22899812ec5
hmilroy <h.milroyperera@gmail.com>
2025-03-25 12:22:18 +0200

    new branch for products

--- cypress/e2e/ui-tests/1-login-tests.js
+++ cypress/e2e/ui-tests/1-login-tests.js
@@ -141,9 +141,3 @@ describe("Login page", () => {
 //     cy.get('.todo-list li')
 //       .should('have.length', 1)
 //       .should('not.have.text', 'Pay electric bill')
-
-//     // Finally, make sure that the clear button no longer exists.
-//     cy.contains('Clear completed').should('not.exist')
-//   })
-// })
-//})
