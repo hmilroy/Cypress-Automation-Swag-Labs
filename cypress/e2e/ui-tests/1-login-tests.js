343d656a8bc8450d86c5483dd77f7df6d36913ce
hmilroy <h.milroyperera@gmail.com>
2025-03-26 15:00:26 +0200

    Add date

--- cypress/e2e/ui-tests/1-login-tests.js
+++ cypress/e2e/ui-tests/1-login-tests.js
@@ -64,7 +64,7 @@ describe("Login page", () => {
   // // We can go even further and check that the default todos each contain
   // // the correct text. We use the `first` and `last` functions
   // // to get just the first and last matched elements individually,
-  // // and then perform an assertion with `should`.
+
   // cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
   // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
 });
