85147dc5a14954e2e66506e81116a134fa9e498d
hmilroy <h.milroyperera@gmail.com>
2025-03-31 15:01:50 +0300

    Allure reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
+// cypress.config.js
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.js",
+    baseUrl: "https://www.saucedemo.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Initialize Allure plugin
+      allureWriter(on, config);
+      return config;
     },
+  },
+  // Enable Allure in Cypress environment
+  env: {
+    allure: true,
   },
 });
